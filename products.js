3f5f6fc206dcb49bc4c3b26a6deea01ba4c07a9d
deveshdaswani1 <165415688+deveshdaswani1@users.noreply.github.com>
2024-03-29 22:55:10 +0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,107 @@
+let products = [
+    {
+        "id": 1001,
+        "subject": "Soccer ball",
+        "Brand": "adidas",
+        "size": "5",
+        "price": 120,
+        "image": "images/soccer.jpg",
+        "space": "5",
+        "availableInventory": 5,
+        "rating": 4
+
+
+    },
+    {
+        "id": 1002,
+        "subject": "Boxing gloves",
+        "Brand": "Venum",
+        "price": 140,
+        "image": "images/boxing.jpg",
+        "space": "4",
+        "availableInventory": 4,
+        "rating": 4
+
+    },
+    {
+        "id": 1003,
+        "subject": "Cricket bat",
+        "Brand": "Nike",
+        "price": 160,
+        "image": "images/cricket.jpg",
+        "space": "5",
+        "availableInventory": 5,
+        "rating": 3
+    },
+    {
+        "id": 1004,
+        "subject": "Tennis Racket",
+        "Brand": "HEAD",
+        "price": 350,
+        "image": "images/tennis.jpg",
+        "space": "3",
+        "availableInventory": 3,
+        "rating": 4
+    },
+    {
+        "id": 1005,
+        "subject": "Badminton racket",
+        "Brand": "Yonex",
+        "price": 100,
+        "image": "images/badminton.jpg",
+        "space": "4",
+        "availableInventory": 4,
+        "rating": 4
+    },
+    {
+        "id": 1006,
+        "subject": "Basketball",
+        "Brand": "Spalding",
+        "price": 350,
+        "image": "images/basketball.jpg",
+        "space": "4",
+        "availableInventory": 4,
+        "rating": 4
+    },
+    {
+        "id": 1007,
+        "subject": "American football",
+        "Brand": "Wilson",
+        "price": 350,
+        "image": "images/american.jpg",
+        "space": "4",
+        "availableInventory": 4,
+        "rating": 4
+    },
+    {
+        "id": 1008,
+        "subject": "Football cleats",
+        "Brand": "Nike",
+        "price": 750,
+        "image": "images/shoes.jpg",
+        "space": "4",
+        "availableInventory": 4,
+        "rating": 4
+    },
+    {
+        "id": 1009,
+        "subject": "Goalpost",
+        "Brand": "Cozy Trix",
+        "price": 400,
+        "image": "images/goalpost.jpg",
+        "space": "4",
+        "availableInventory": 4,
+        "rating": 4
+    },
+    {
+        "id": 1010,
+        "subject": "Sports Cap",
+        "Brand": "Nike",
+        "price": 90,
+        "image": "images/cap.jpg",
+        "space": "4",
+        "availableInventory": 4,
+        "rating": 4
+    },
+
+]
